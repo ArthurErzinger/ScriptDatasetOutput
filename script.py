ca870ea916b8aedbc1ba38a536a769210e1df0b6
arthur <arthurerzingerdelima@gmail.com>
2024-08-22 14:25:28 -0300

    Script roda em windows

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
+'''
+Script escrito em python para a organização de outputs de co-simulações com o software DOMUS em formatos de tabela.
+
+Como usar:
+
+-Baixar o pandas (pip install pandas)
+-Baixar o openpyxl (pip install openpyxl)
+-Copie o caminho da pasta da sua simulação
+-Cole dentro das aspas de "diretorioRaiz" no inicio do código
+Desenvolvido por Arthur e Micael.
+
+PUCPR.
+'''
+
+import platform
 import os
 import fnmatch as fn
 import pandas as pd
@@ -17,11 +32,12 @@ def caminhos(root, nome):
 
     return caminhoDoArquivo, zona, iteracao, simulacao
 
-# Caminho da pasta raiz que você deseja iterar
-## para linux '/home/diretorio1/diretorio2/...diretorion/'
+diretorioRaiz = r'/home/arthurerzinger/arthur/resultados'
 
-##para Windows 'disco:\\diretorio1\\diretorio2\\...diretorion\\'
-diretorioRaiz = ''
+if (platform.system() == "Windows"):
+    diretorioRaiz = diretorioRaiz.replace('\\', '\\\\')
+
+
 # Lista para armazenar linhas do CSV
 csvTUP = []
 csvCONS = []
